b52eca0ebd7302dd9771282e982f8498dd55624b
Ahmed Raihan <152580161+notraihanahmed@users.noreply.github.com>
2024-10-16 14:29:13 +0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = 'my-cache-v2';
+const CACHE_NAME = 'pwa-cache-v1';
 const urlsToCache = [
   '/',
   '/index.html',
   '/style.css',
-  '/images/logo name.png',
-  '/images/raihan-ahmed(1)small.webp'
+  '/images/logo.png',
+  '/images/raihan-ahmed(1)small.webp',
 ];
 self.addEventListener('install', (event) => {
   event.waitUntil(
@@ -15,28 +15,9 @@ self.addEventListener('install', (event) => {
 });
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request).then((fetchResponse) => {
-        // Cache the fetched response for future use
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request).then((response) => {
+        // Cache the new resource for future use
         return caches.open(CACHE_NAME).then((cache) => {
-          cache.put(event.request, fetchResponse.clone());
-          return fetchResponse;
-        });
-      });
-    })
-  );
-});
-self.addEventListener('activate', (event) => {
-  const cacheWhitelist = [CACHE_NAME];
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cacheName) => {
-          if (!cacheWhitelist.includes(cacheName)) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
-});
+          cache.put(event.request, response.clone());
+          return response;
